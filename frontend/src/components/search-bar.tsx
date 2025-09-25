31c65e3e672ec1f19fda9acbd34a7a269c3a8f6c
rubengpr <61808374+rubengpr@users.noreply.github.com>
2025-09-25 19:27:58 +0200

    fix: add background to input

--- frontend/src/components/search-bar.tsx
+++ frontend/src/components/search-bar.tsx
@@ -48,7 +48,7 @@ export default function SearchBar({
           value={localSearchTerm}
           onChange={handleInputChange}
           placeholder={placeholder}
-          className="block w-full pl-12 pr-12 py-2 border-2 border-indigo-300 rounded-xl text-base placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-colors"
+          className="block w-full pl-12 pr-12 py-2 bg-white border-2 border-indigo-300 rounded-xl text-base placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-colors"
         />
         {localSearchTerm && (
           <button
