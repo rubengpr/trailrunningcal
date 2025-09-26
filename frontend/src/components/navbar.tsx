51a31a26e4aa8838ad6e739f210f027a3630bf75
rubengpr <61808374+rubengpr@users.noreply.github.com>
2025-09-27 00:32:13 +0200

    fix: add py-4 to navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/trc-logo.svg';
 
 export default function Navbar() {
   return (
-    <header className="w-full bg-white border-b border-indigo-100/60 px-4 sm:px-6 lg:px-8">
+    <header className="w-full bg-white border-b border-indigo-100/60 px-4 py-4 sm:px-6 lg:px-8">
       <div className="flex justify-between items-center">
         <Link to="/" className="flex items-center gap-3">
           <img className="w-10 h-10" src={logo} alt="Trail Running Calendar" />
