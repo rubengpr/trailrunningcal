e1238fedfe7d7c6698998f15dc071e345e857922
rubengpr <61808374+rubengpr@users.noreply.github.com>
2025-12-27 13:14:30 +0100

    style: adjust layout and typography in RaceCard component for improved readability and visual consistency

--- frontend/components/race-card.tsx
+++ frontend/components/race-card.tsx
@@ -48,31 +48,32 @@ export default function RaceCard({
       <div className="absolute inset-0 bg-linear-to-b from-transparent from-30% to-black" />
 
       {/* Content - All info at bottom */}
-      <div className="relative h-full flex flex-col justify-end p-6 sm:p-8 lg:p-12">
-        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-end gap-4">
+      <div className="relative h-full flex flex-col justify-end p-5 sm:p-8 lg:p-12">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-end gap-2">
           {/* Race Name, Distance and Elevation - Bottom Left */}
           <div className="text-white">
             <h3
-              className={`${montserratExtrabold.className} text-3xl sm:text-4xl lg:text-5xl xl:text-6xl text-white mb-2`}
+              className={`${montserratExtrabold.className} text-xl sm:text-4xl lg:text-5xl xl:text-6xl text-white mb-1 sm:mb-2`}
             >
               {raceName}
             </h3>
             <p
-              className={`${montserratMedium.className} text-md sm:text-lg lg:text-xl`}
+              className={`${montserratMedium.className} text-xs sm:text-lg lg:text-xl`}
             >
               {distance} - {elevationGain}
             </p>
           </div>
 
           {/* Location and Date - Bottom Right */}
-          <div className="text-white text-right">
+          <div className="flex flex-row sm:flex-col text-white text-right">
             <p
-              className={`${montserratMedium.className} text-md sm:text-lg lg:text-xl`}
+              className={`${montserratMedium.className} text-xs sm:text-lg lg:text-xl`}
             >
               {location}
             </p>
+            <span className="text-xs">&nbsp;-&nbsp;</span>
             <p
-              className={`${montserratMedium.className} text-md sm:text-lg lg:text-xl`}
+              className={`${montserratMedium.className} text-xs sm:text-lg lg:text-xl`}
             >
               {date}
             </p>
