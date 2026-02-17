40e960dad6c96bb522df61cd298811269c073963
rubengpr <61808374+rubengpr@users.noreply.github.com>
2026-02-17 13:10:33 +0100

    feat: conditionally render RaceOrganizerClaimCard based on organizerId
    - Update RacePage component to only display the RaceOrganizerClaimCard when there is no organizerId present in raceData. This change improves the user interface by preventing unnecessary display of the claim card when an organizer is already assigned to the race.

--- frontend/app/[locale]/carrera/[race]/page.tsx
+++ frontend/app/[locale]/carrera/[race]/page.tsx
@@ -250,15 +250,17 @@ export default async function RacePage({
             />
           )}*/}
 
-        <RaceOrganizerClaimCard
-          title={tRace('organizerCard.title')}
-          titleMobile={tRace('organizerCard.titleMobile')}
-          description={tRace('organizerCard.description')}
-          descriptionMobile={tRace('organizerCard.descriptionMobile')}
-          benefits={tRace('organizerCard.benefits')}
-          claimButton={tRace('organizerCard.claimButton')}
-          raceName={raceData.name}
-        />
+        {!raceData.organizerId && (
+          <RaceOrganizerClaimCard
+            title={tRace('organizerCard.title')}
+            titleMobile={tRace('organizerCard.titleMobile')}
+            description={tRace('organizerCard.description')}
+            descriptionMobile={tRace('organizerCard.descriptionMobile')}
+            benefits={tRace('organizerCard.benefits')}
+            claimButton={tRace('organizerCard.claimButton')}
+            raceName={raceData.name}
+          />
+        )}
       </div>
     </div>
   );
