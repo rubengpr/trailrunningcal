46c582a192960bb552c9cf5aa9855d82cd835965
mnavid <learningbizz@gmail.com>
2026-05-14 12:14:02 +0200

    fix latitude telemetry

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,17 +1,26 @@
-import { LatitudeSpanProcessor } from '@latitude-data/telemetry';
+import OpenAI from 'openai';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import {
+  LatitudeSpanProcessor,
+  registerLatitudeInstrumentations,
+} from '@latitude-data/telemetry';
 
 export async function register() {
+  const apiKey = process.env.LATITUDE_API_KEY;
+  const projectSlug = process.env.LATITUDE_PROJECT_SLUG;
+
+  if (!apiKey || !projectSlug) return;
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const provider = new NodeTracerProvider({
-      spanProcessors: [
-        new LatitudeSpanProcessor(
-          process.env.LATITUDE_API_KEY!,
-          process.env.LATITUDE_PROJECT_SLUG!,
-        ),
-      ],
+      spanProcessors: [new LatitudeSpanProcessor(apiKey, projectSlug)],
     });
-
     provider.register();
+
+    await registerLatitudeInstrumentations({
+      instrumentations: ['openai'],
+      modules: { openai: OpenAI },
+      tracerProvider: provider,
+    });
   }
 }
